aa8fcb042558d395a8547d20c5f25a61d70728fd
tiagokodama <tiagokodama@ciandt.com>
2022-10-05 18:27:58 -0300

    renamed folder

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,7 +3,7 @@ const { ApolloServer, gql } = require('apollo-server');
 const { importSchema } = require('graphql-import');
 
 const schemaPath = './Routes/schema/index.graphql';
-const resolvers = require('./Routes/resolvers');
+const resolvers = require('./routes/resolvers');
 
 const server = new ApolloServer({
   typeDefs: importSchema(schemaPath),
